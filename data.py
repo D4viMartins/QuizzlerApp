e7197da21ff2d6d317e6d89f265c18a8875163f1
D4viMartinsLima <martinslimafkc@gmail.com>
2025-10-18 05:53:31 -0300

    Project  finished

--- data.py
+++ data.py
@@ -13,5 +13,5 @@ data = response.json()
 
 question_data = data["results"]
 
-print(question_data)
+
 
--- ui.py
+++ ui.py
@@ -1,2 +1,73 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+
 THEME_COLOR = "#375362"
+
+class QuizzInterface:
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+        self.window = Tk()
+        self.window.title("Quizzlet")
+        self.window.config(pady=20, padx=20, bg=THEME_COLOR)
+
+        self.canvas = Canvas(width=300, height=250, bg="white")
+        self.canvas.grid(row=1, column=0,columnspan=2, pady=50)
+
+        self.score_label =  Label(text="Score: 0", bg=THEME_COLOR, fg="white")
+        self.score_label.grid(row=0, column=1)
+
+        # self.question = Label(text="Aqui é a pergunta", bg="white")
+        # self.question.grid(row=1, column=0,columnspan=2)
+        self.question_text = self.canvas.create_text(150,
+                                                     125,
+                                                     width= 280,
+                                                     text="Some text",
+                                                     fill=THEME_COLOR,
+                                                     font=("Arial", 20, "italic")
+                                                     )
+
+        true_image = PhotoImage(file="images/true.png")
+        self.true_button = Button(image=true_image, highlightthickness=0, command=self.right_answer)
+        self.true_button.grid(row=2, column=0)
+
+        false_image = PhotoImage(file="images/false.png")
+        self.false_button = Button(image=false_image, highlightthickness=0, pady=40, command=self.wrong_answer)
+        self.false_button.grid(row=2, column=1)
+
+        self.get_next_question()
+
+        self.window.mainloop()
+
+    #ANOTAÇÂO: Basicamente tudo que foi criado possui um "self." torna aquele código em uma propriedade que pode ser acessada
+        #em qualquer lugar da classe. Nas linhas em que estão sendo criadas as imagens não há a criação de nenhuma propriedade
+         # e não serão utilizadas em nenhum lugar além daquele em especifico que irá configurar as imagens
+
+    def get_next_question(self):
+        self.canvas.config(bg="white")
+        if self.quiz.still_has_questions():
+            self.score_label.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question_text, text=q_text)
+        else:
+            self.canvas.itemconfig(self.question_text, text="You have reached the end of the Quiz")
+            self.true_button.config(state="disabled")
+            self.false_button.config(state="disabled")
+
+    def right_answer(self):
+        is_right = self.quiz.check_answer("True")
+        self.give_feedback(is_right)
+
+    def wrong_answer(self):
+        is_right = self.quiz.check_answer("False")
+        self.give_feedback(is_right)
+
+    def give_feedback(self, is_right):
+        if is_right:
+            self.canvas.config(bg="green")
+        else:
+            self.canvas.config(bg= "red")
+        self.window.after(1000, self.get_next_question)
+
+
+
 
